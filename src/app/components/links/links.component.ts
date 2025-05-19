7c986eb4d4cebf6f354d02f0b7573b41cb518263
Gabriel Silva <c0681668@vale.com>
2025-05-19 10:39:12 -0300

    feat: add first links

--- src/app/components/links/links.component.ts
+++ src/app/components/links/links.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { LinksJson } from 'src/app/types/link.type';
+import { LinksService } from './../../services/links.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-links',
   templateUrl: './links.component.html',
   styleUrls: ['./links.component.scss']
 })
-export class LinksComponent {
+export class LinksComponent implements OnInit {
+  links: LinksJson = [];
 
+  constructor(private linksService: LinksService) {}
+
+  ngOnInit(): void {
+    this.linksService.getJsonData().subscribe(data => {
+      this.links = data.links;
+    });
+  }
 }
---
+++ src/app/types/link.type.ts
@@ -0,0 +1,4 @@
+export type LinksJson = {
+  title: string,
+  url: string,
+}[]
